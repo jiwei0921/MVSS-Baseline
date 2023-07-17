15db69f772d99c5c51b82edb3174d72da8ec6814
wei ji <weiji.dlut@gmail.com>
2023-07-16 20:11:30 -0700

    Update train.sh

--- MVNet/train.sh
+++ MVNet/train.sh
@@ -4,4 +4,4 @@
 #CUDA_VISIBLE_DEVICES=0,1 python main.py --backbone deeplab50 --training --baseline-mode --gpus 2 --lr-start 2e-4 --lr-strategy plateau_08 --num-epochs 150 --batch-size 2 --stm-queue-size 3 --sample-rate 3 --savedir save/training_base_deeplab
 
 # Stage2: Training with memory
-CUDA_VISIBLE_DEVICES=0,1 python main.py --backbone deeplab50 --weights save/training_base_deeplab/model_best.pth --training --gpus 2 --lr-start 2e-4 --lr-strategy plateau_08 --num-epochs 200 --batch-size 2 --stm-queue-size 3 --sample-rate 3 --savedir save/training_msa_deeplab
+CUDA_VISIBLE_DEVICES=0,1 python main.py --backbone deeplab50 --weights save/training_base_deeplab/model_base.pth --training --gpus 2 --lr-start 2e-4 --lr-strategy plateau_08 --num-epochs 200 --batch-size 2 --stm-queue-size 3 --sample-rate 3 --savedir save/training_msa_deeplab
