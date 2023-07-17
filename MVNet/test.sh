a92793d23d63e8a9dcf563ddc206b4ad789d9cc0
wei ji <weiji.dlut@gmail.com>
2023-07-16 20:11:14 -0700

    Update test.sh

--- MVNet/test.sh
+++ MVNet/test.sh
@@ -1,7 +1,7 @@
 # Model Inference
 
 # Inference for two-stream baseline model
-#python main.py --eval-mode --split-mode test --backbone deeplab50 --weights save/training_base_deeplab/model_best.pth --stm-queue-size 3 --sample-rate 3 --savedir save/eval_base_deeplab --baseline-mode
+#python main.py --eval-mode --split-mode test --backbone deeplab50 --weights save/training_base_deeplab/model_base.pth --stm-queue-size 3 --sample-rate 3 --savedir save/eval_base_deeplab --baseline-mode
 
 # Inference for the MVNet model
 python main.py --eval-mode --split-mode test --backbone deeplab50 --weights save/training_msa_deeplab/model_best.pth --stm-queue-size 3 --sample-rate 3 --savedir save/eval_msa_deeplab
